a8c0ea8b86e2143d4ac5be9d77381a4e99572c77
Nicolas Jesenberger <nicolas@MacBook-Air-de-Nicolas.local>
2023-12-08 16:52:50 +0100

    fix: quotes

--- components/modals/AddUserModal/index.tsx
+++ components/modals/AddUserModal/index.tsx
@@ -11,7 +11,7 @@ type Ref = {
 } | null;
 
 interface ModalProps extends React.HTMLAttributes<HTMLDialogElement> {
-  onConfirm (user: Omit<User, 'id'>): Promise<Omit<User, "id">>;
+  onConfirm (user: Omit<User, 'id'>): Promise<Omit<User, 'id'>>;
 }
 
 const AddUserModal = forwardRef<Ref, ModalProps>(
